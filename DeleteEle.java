14dd8140a354cb587026d5bd1388148ce242f176
Raushan Singh <84634774+Iraushan1@users.noreply.github.com>
2023-06-02 19:42:49 +0530

    Add files via upload

---
+++ DeleteEle.java
@@ -0,0 +1,15 @@
+package array;
+
+import java.util.Scanner;
+public class DeleteEle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array size : ");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter elemant to delete :");
+		for(int i=0; i<arr.length; i++) {
+			
+		}
+	}	
+}
